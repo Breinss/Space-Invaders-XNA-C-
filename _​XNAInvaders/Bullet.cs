a3f955ece2aa1dc4a290dd24cadcac29318edbdc
Breandan Wehmuller <breandan.wehmuller@hva.nlmkdir>
2015-02-16 23:07:56 +0100

    Revert "Revert "First Commit""
    This reverts commit d0f2b0a7eec56598b4fdb5a614ca6d5df54ad81c.

---
+++ _​XNAInvaders/Bullet.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XNAInvaders
+{
+    public class Bullet
+    {
+        public Boolean isFired = true;
+        public Vector2 position;
+        Vector2 velocity;
+        public Texture2D texture;
+
+        public Bullet(Vector2 _position)
+        {
+            texture = Global.content.Load<Texture2D>("bullet");
+            position = _position;
+            Init();
+
+        }
+
+        public void Init()
+        {
+            velocity.Y = 5f;
+            
+        }
+
+        public void Update()
+        {
+            
+            position -= velocity;
+           
+              //  if (position.Y < 0)
+              //  {
+               //     theGame1.RemoveBullet();
+                //}
+                
+            
+            
+
+            
+
+        }
+
+        public void Draw()
+        {
+            Global.spriteBatch.Draw(texture,position,Color.White);
+        }
+
+        public void Reset()
+        {
+            isFired = false;
+            //position.X = -1000;
+
+        }
+
+//        public Boolean OverlapsInvader(Invader anInvader)
+//        {
+//        }
+
+    }
+}
